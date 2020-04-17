3c58e37e20379ff583fdfb45de4a063c56f7671f
CesarVE <thecesarvargas@gmail.com>
2020-04-16 20:02:56 -0500

    Agregando tipos de datos simples al modelo y desplegarlos a la vista

--- empleos/src/main/java/com/vaescode/empleos/controller/HomeController.java
+++ empleos/src/main/java/com/vaescode/empleos/controller/HomeController.java
@@ -10,10 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	/**
+	 * @param model
+	 * @return
+	 */
 	@GetMapping({"/","home"})
 	public String mostrarHome(Model model) {
-		model.addAttribute("mensaje", "Bienvenidos a Empleos App");
-		model.addAttribute("fecha", new Date());
+		/*model.addAttribute("mensaje", "Bienvenidos a Empleos App");
+		model.addAttribute("fecha", new Date());*/
+		String nombre= "Auxiliar de contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000.0;
+		boolean vigente = true;
+		model.addAttribute("nombre",nombre);
+		model.addAttribute("fecha", fechaPub);
+		model.addAttribute("salario",salario);
+		model.addAttribute("vigente",vigente);
+		
 		return "home";
 	}
 	
