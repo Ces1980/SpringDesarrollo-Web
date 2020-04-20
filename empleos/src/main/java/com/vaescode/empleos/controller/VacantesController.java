2b5366f88ca70e45030c0d833c2ee43c1ab99e40
CesarVE <thecesarvargas@gmail.com>
2020-04-20 01:15:13 -0500

    @RequestParam: ejemplo de peticion HTTP GET

--- empleos/src/main/java/com/vaescode/empleos/controller/VacantesController.java
+++ empleos/src/main/java/com/vaescode/empleos/controller/VacantesController.java
@@ -3,13 +3,20 @@ package com.vaescode.empleos.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
+	
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam("id") int idVacante, Model model) {
+		model.addAttribute("id", idVacante);
+		System.out.println("Borrando vacante con id: " + idVacante);
+		return "mensaje";
+	}
 
 	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
