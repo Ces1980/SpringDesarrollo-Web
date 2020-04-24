499dc500a1522faa09de9cd0cefe7d861bbc1ada
CesarVE <thecesarvargas@gmail.com>
2020-04-24 01:19:19 -0500

    Configurar método en controlador y formulario HTML para crear una vacante

--- empleos/src/main/java/com/vaescode/empleos/controller/VacantesController.java
+++ empleos/src/main/java/com/vaescode/empleos/controller/VacantesController.java
@@ -2,6 +2,7 @@ package com.vaescode.empleos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +19,28 @@ public class VacantesController {
 	
 	@Autowired
 	private IVacanteService serviceVacante;
+	
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion,
+			              @RequestParam("estatus") String estatus, @RequestParam("fecha") String fecha, 
+			              @RequestParam("destacado") int destacado,  @RequestParam("salario") double salario,
+			              @RequestParam("detalles") String detalles) {
+			   
+		/*Para verificar en consola que llegaron los datos coorectamente*/
+		System.out.println("Nombre Vacante: " + nombre);
+		System.out.println("Descripción: " + descripcion);
+		System.out.println("Estatus: " + estatus);
+		System.out.println("Fecha publicación: " + fecha);
+		System.out.println("Destacado: " + destacado);
+		System.out.println("Salario: " + salario);
+		System.out.println("Detalles: " + detalles);
+		return "vacantes/listVacantes";
+	}
 	
 	@GetMapping("/delete")
 	public String eliminar(@RequestParam("id") int idVacante, Model model) {
