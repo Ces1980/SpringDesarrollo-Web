b5e33bb7366cb0b0bd7a427be437ab8e72760025
CesarVE <thecesarvargas@gmail.com>
2020-04-29 21:27:20 -0500

    Creando la clase categoria, la interfaz IcategoriaSerivice, clase, CategoriaServiceImpl y las vistas listCategoria - formCategoria

--- empleos/src/main/java/com/vaescode/empleos/controller/CategoriasController.java
+++ empleos/src/main/java/com/vaescode/empleos/controller/CategoriasController.java
@@ -1,38 +1,54 @@
 package com.vaescode.empleos.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.vaescode.empleos.model.Categoria;
+import com.vaescode.empleos.service.ICategoriasService;
 
 @Controller
-/*Se inscribe a nivel clase el prefijo /categorias para construir la URL
- * */
+/*
+ * Se inscribe a nivel clase el prefijo /categorias para construir la URL
+ */
 @RequestMapping("/categorias")
 public class CategoriasController {
 
-	
-		@GetMapping("/index")
-		public String mostrarIndex(Model model) {
-			return "categorias/listCategorias";
-		}
-		
-		 @GetMapping("/create")
-		public String crear() {
-		return "categorias/formCategoria";
-		}
-		 
-		 
-		//@PostMapping("/save")
-		 @RequestMapping(value="/save", method = RequestMethod.POST)
-		public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
-			System.out.println("Categoria: " +nombre);
-			System.out.println("Descripción: " +descripcion);
-			
-			
+	@Autowired
+	private ICategoriasService serviceCategorias;
+
+	@GetMapping(value = "/index")
+	public String mostrarIndex(Model model) {
+		List<Categoria> lista = serviceCategorias.buscarTodas();
+		model.addAttribute("categorias", lista);
 		return "categorias/listCategorias";
-		}
+	}
+
+	@GetMapping("/create")
+	public String crear(Categoria categoria) {
+		return "categorias/formCategoria";
+	}
+
+	@PostMapping("/save")
+	public String guardar(Categoria categoria, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()){		
+			System.out.println("Existieron errores");
+			return "categorias/formCategoria";
+		}	
+		
+		// Guadamos el objeto categoria en la bd
+		serviceCategorias.guardar(categoria);
+		attributes.addFlashAttribute("msg", "Los datos de la categoría fueron guardados!");		
+		return "redirect:/categorias/index";
+	}
+	
 }
